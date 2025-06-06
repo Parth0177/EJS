0e526a71ed361c2df4f4b34a2a09258f63c0babe
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-07 00:33:41 +0530

    Add Express server setup and interpolation view

---
+++ interpolation.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app= express();
+const PORT=5004;
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on ${PORT}`)
+})
+
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+  res.render('interpolation')
+})
+
+app.get('/rolldice',(req,res)=>{
+  
+})
